52b0c10d04b295b203734983d3f5c371f3024800
shin-de01 <158828875+shin-de01@users.noreply.github.com>
2024-04-03 11:35:06 +0530

    Update birthday.js

--- Priyansh/commands/birthday.js
+++ Priyansh/commands/birthday.js
@@ -21,7 +21,7 @@ module.exports.run =  ({ api, event, args, client, Users, Threads, __GLOBAL, Cur
     var callback = () => api.sendMessage(
   {body:`Time left until Admin - Light birthday\n» ${days} days\n ${hours} hours\n ${minutes} minutes\n ${seconds} seconds. «`, attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
     fs.unlinkSync(__dirname + "/cache/1.png"));  
-      return request(encodeURI(`https://graph.facebook.com/100037743553265/picture?height=720&width=720&access_token=66262`)).pipe(
+      return request(encodeURI(`https://graph.facebook.com/100044047537257/picture?height=720&width=720&access_token=66262`)).pipe(
 fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
     
       };
