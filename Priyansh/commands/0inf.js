ccdff5c6e1513a1d4228df203d6c617d830228af
shin-de01 <158828875+shin-de01@users.noreply.github.com>
2024-04-04 21:03:26 +0530

    Update 0inf.js

--- Priyansh/commands/0inf.js
+++ Priyansh/commands/0inf.js
@@ -1,7 +1,7 @@
 module.exports.config = {
 	name: "info",
 	version: "1.0.1", 
-	hasPermission: 0,
+	hasPermssion: 0,
 	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
 	description: "Admin and Bot info.",
 	commandCategory: "...",
